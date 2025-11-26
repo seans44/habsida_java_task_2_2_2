944576cddd0bdf4c80d6887865abf1555a448176
seans44 <kyon.s44@gmail.com>
2025-11-26 18:47:26 +0900

    First commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,12 +3,8 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-
-import java.util.List;
 
 @Controller
 public class CarController {
